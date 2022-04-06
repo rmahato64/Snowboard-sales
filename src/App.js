bcd3be5e67f7d12f48c6e9fc3dad1da24c6deba7
Rakesh Mahato <rmahato64@gmail.com>
2022-04-06 20:59:09 +0300

    Snowboard sales according to country along with bar chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 function App() {
+  const data = [
+    {
+      country: "Finland",
+      sales: 400,
+    },
+    {
+      country: "France",
+      sales: 100,
+    },
+    {
+      country: "Brazil",
+      sales: 70,
+    },
+  ];
+
+  const [country, setCountry] = useState("");
+  const [filteredData, setFilteredData] = useState(data);
+  const [selectedData, setSelectedData] = useState("");
+
+  const handleSubmit = () => {
+    const selectedCountry = data.find(
+      (d) => d.country.toUpperCase() === country.toUpperCase()
+    );
+    if(country && selectedCountry){
+      setFilteredData([selectedCountry])
+    }
+    else if(country && !selectedCountry){
+      setFilteredData([])
+    }
+    else{
+      setFilteredData(data)
+    }
+    setSelectedData(selectedCountry);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>Find snowboard sales for Finland, France and Brazil.</h1>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Enter country name"
+          onChange={(e) => setCountry(e.target.value)}
+          value={country}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+
+      {selectedData && (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Snowboard sales for {selectedData.country} in 2022 is{" "}
+          {selectedData.sales}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      )}
+      {!filteredData.length && <p>Invalid input.</p>}
+
+      {filteredData.length > 0 && (
+        <BarChart
+          width={500}
+          height={300}
+          data={filteredData}
+          barSize={10}
         >
-          Learn React
-        </a>
-      </header>
+          <XAxis
+            dataKey="country"
+            scale="point"
+            padding={{ left: 10, right: 10 }}
+            label={{
+              value: "Country",
+              position: "insideBottomRight",
+              offset: 0,
+            }}
+          />
+          <YAxis
+            label={{ value: "Sales", position: "insideLeft", angle: -90 }}
+          />
+          <Tooltip />
+          <Legend />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Bar dataKey="sales" fill="#8884d8" background={{ fill: "#eee" }} />
+        </BarChart>
+      )}
     </div>
   );
 }
